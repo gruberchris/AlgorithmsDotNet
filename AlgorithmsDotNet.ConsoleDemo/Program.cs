b4ae8e1d59c496ab7a0011b56be57c3a68a866cd
Chris Gruber <chris@chrisgruber.com>
2017-03-19 17:42:01 -0500

    implemented insertion sort

---
+++ AlgorithmsDotNet.ConsoleDemo/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace AlgorithmsDotNet.ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var runInsertionSortOnIntegerArrayResult = RunInsertionSortOnIntegerArray();
+
+            foreach (var i in runInsertionSortOnIntegerArrayResult.Item1)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.WriteLine(runInsertionSortOnIntegerArrayResult.Item2);
+
+            Console.WriteLine(string.Empty);
+
+            var runInsertSortOnStringArrayResult = RunInsertionSortOnStringArray();
+
+            foreach (string s in runInsertSortOnStringArrayResult.Item1)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine(runInsertSortOnStringArrayResult.Item2);
+        }
+
+        static Tuple<int[], string> RunInsertionSortOnIntegerArray()
+        {
+            int[] unsortedArray = { 29, 27, 20, 28, 24, 26, 9, 7, 10, 8, 4, 6, 21, 22, 25, 23, 1, 2, 5, 3 };
+
+            var startDateTime = DateTime.Now;
+
+            var sortedArray = unsortedArray.InsertionSort((a, b) => (a > b));
+
+            var endDateTime = DateTime.Now;
+
+            var elapsed = endDateTime - startDateTime;
+
+            string resultMessage = $"*** Elapsed: {elapsed.Milliseconds.ToString()} ms";
+
+            return Tuple.Create(sortedArray, resultMessage);
+        }
+
+        static Tuple<string[], string> RunInsertionSortOnStringArray()
+        {
+            string[] unsortedArray = { "halt", "plane", "train", "analyzed", "HAL", "oddesey", "brakes", "mobile", "analyzing", "halted", "be rigth back", "123 Street" };
+
+            var startDateTime = DateTime.Now;
+
+            var sortedArray = unsortedArray.InsertionSort((a, b) => a.CompareTo(b) > 0);
+
+            var endDateTime = DateTime.Now;
+
+            var elapsed = endDateTime - startDateTime;
+
+            string resultMessage = $"*** Elapsed: {elapsed.Milliseconds.ToString()} ms";
+
+            return Tuple.Create(sortedArray, resultMessage);
+        }
+    }
+}
---
+++ AlgorithmsDotNet/Sorting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsDotNet
+{
+    public static class Sorting
+    {
+        public static T[] InsertionSort<T> (this T[] collection, Func<T, T, bool> compare)
+        {
+            for (var count = 0; count < collection.Count() - 1; count++)
+            {
+                var nextElement = count + 1;
+
+                while (nextElement > 0)
+                {
+                    if (compare(collection[nextElement - 1], collection[nextElement]))
+                    {
+                        var swappingElement = collection[nextElement - 1];
+                        collection[nextElement - 1] = collection[nextElement];
+                        collection[nextElement] = swappingElement;
+                    }
+
+                    nextElement--;
+                }
+            }
+
+            return collection;
+        }
+    }
+}
